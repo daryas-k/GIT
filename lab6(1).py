2ce5ab493567513763e15da01528d8f71604a14d
Карпець Дар'я <113431974+daryas-k@users.noreply.github.com>
2022-10-23 18:47:30 +0300

    Update lab6(1).py

--- lab6(1).py
+++ lab6(1).py
@@ -1,12 +1,13 @@
 import math
-x = float(input('Задайте x'))
 a = int(input('Задайте a'))
 b = int(input('Задайте b'))
-h = int(input('Задайте h'))
+c = int(input('Задайте c'))
+x=a
 
 
-
-for i in range(a, b, h):
+for i in range(100):
     f = 1 / (math.pow(x, 2) + 1) + math.pow(x, 2)
-    print("%i x=%.1f     y=%.3f"%(i,x,f))
-    x=x+h
+    x=x+c
+    print('x =', x, 'f=',f)
+    if x>b:
+        break
